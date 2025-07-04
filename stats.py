a08a14861506bc99007fdd4622095a12ff77ef48
egrks <egrksnew@yandex.ru>
2025-07-04 20:52:14 +0200

    updated book bot with like some shit

---
+++ stats.py
@@ -0,0 +1,26 @@
+def get_book_text(flpth):
+    with open(flpth) as f:
+        file_text = f.read()
+        return file_text
+    
+def wordcount(flpth):
+    split_text = get_book_text(flpth).split()
+    k = 0
+    for i in split_text:
+        k += 1
+    return f"{k} words found in the document"
+
+def charcount(flpth):
+    char_dict = {}
+    text = get_book_text(flpth)
+    lower_text = text.lower()
+    split_text = lower_text.split()
+    
+    for i in split_text:
+        character_list = list(i)
+        for character in character_list:
+            if character in char_dict:
+                char_dict[character] += 1
+            else:
+                char_dict[character] = 1
+    return char_dict
